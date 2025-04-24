bf47a658def2fdf819e87486cc736a71400d14ae
Vishniew <vishnukesari14@gmail.com>
2025-04-24 12:13:13 +0530

    Added code for Toggling og LED for every sec.

--- Sample_ws/V_app/Core/Src/main.c
+++ Sample_ws/V_app/Core/Src/main.c
@@ -95,7 +95,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin(GPIOI, LED);
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
